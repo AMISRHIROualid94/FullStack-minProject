0bd84a2e0a2de4951bc52ebedf5de9ea8ede42b9
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-01-12 21:26:15 +0100

    initial post with fake data. closes #9

--- fullstackproject/src/app/services/post.service.ts
+++ fullstackproject/src/app/services/post.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+
+posts:Post[] = [
+  {
+    title:"postTest",
+    content:"Content for post test"
+  }
+]
 
   constructor() { }
 }
