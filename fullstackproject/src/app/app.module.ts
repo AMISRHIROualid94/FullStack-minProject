a70b9ceeef0650457fbd25a378c2ff65b7a3356c
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-01-12 21:18:06 +0100

    add toolbar Navigation. closes #5

--- fullstackproject/src/app/app.module.ts
+++ fullstackproject/src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
+import { HeaderComponent } from './posts/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     PostListComponent,
-    PostItemComponent
+    PostItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
