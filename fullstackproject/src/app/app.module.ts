e3c25a7207f55b837196205e740885ddbc399eb6
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-01-12 21:12:09 +0100

    create components. closes #4

--- fullstackproject/src/app/app.module.ts
+++ fullstackproject/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
